8f851ea119fc102c14bfbcaa8fb4b7b2a025de18
abhidahal <abhidahal7@gmail.com>
2023-08-19 17:08:31 +0545

    PWSH command scripts updated

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -3,9 +3,29 @@ Invoke-Expression (&starship init powershell)
 Set-PSReadLineOption -PredictionSource History
 Set-PSReadLineOption -PredictionViewStyle ListView
 set-alias -name c -value clear
+set-alias -name gti -value git
 set-alias -name q -value exit
+
 function .. { cd .. }
-function mkcd([string]$FileName)
+
+function gco([string]$BranchName)
 {
-    mkdir $FileName && cd $FileName
+    git checkout $BranchName
+}
+function gcob([string]$BranchName)
+{
+    git checkout -b $BranchName
+}
+function campr([string]$LabelName)
+{
+    gh pr create -a @me -r @h3cksamrat -l $LabelName
+}
+function cpr([string]$LabelName)
+{
+    gh pr create -a @me -l $LabelName
+}
+
+function vpr()
+{
+    gh pr view --web
 }
